9842d5faa4a8caf382377d7de3bf650efc90cbf2
Guillermo Morales <morales.guillermo2503@gmail.com>
2024-04-08 21:51:06 -0600

    Inicialización de las aplicaciones del back y el front

---
+++ Back/kitsune_api/src/main/java/com/kitsune/kitsune_api/KitsuneApiApplication.java
@@ -0,0 +1,13 @@
+package com.kitsune.kitsune_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitsuneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitsuneApiApplication.class, args);
+	}
+
+}
