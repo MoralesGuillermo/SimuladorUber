5e1db2c32f2c93146fb6ac343f74f1375ab9d025
Guillermo Morales <morales.guillermo2503@gmail.com>
2024-04-20 17:11:43 -0600

    Agregadas vistas para el manejo de rides de parte del cliente

--- Front/kitsune/routes/web.php
+++ Front/kitsune/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/solicitar-ride', function(){
+    return view('solicitarRide');
+});
+
+Route::get('/solicitar-ride/confirm', function(){
+    return view('esperarSolicitud');
+});
