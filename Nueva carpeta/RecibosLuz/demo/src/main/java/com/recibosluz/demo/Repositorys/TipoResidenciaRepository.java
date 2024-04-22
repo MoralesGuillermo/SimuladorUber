9a0f987f2650e9b1d197edde857175915e1b7faa
danrecarte <109390974+danrecarte@users.noreply.github.com>
2024-04-22 01:40:30 -0600

    Delete Nueva carpeta directory

--- Nueva carpeta/RecibosLuz/demo/src/main/java/com/recibosluz/demo/Repositorys/TipoResidenciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recibosluz.demo.Repositorys;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.recibosluz.demo.Entities.TipoResidencia;
-
-public interface TipoResidenciaRepository extends CrudRepository<TipoResidencia, Integer>{
-
-}
--- Nueva carpeta/RecibosLuz/demo/src/main/java/com/recibosluz/demo/Services/TipoResidenciaService.java
+++
@@ -1,9 +0,0 @@
-package com.recibosluz.demo.Services;
-
-import com.recibosluz.demo.Entities.TipoResidencia;
-
-public interface TipoResidenciaService {
-    
-    public TipoResidencia crearTipoResidencia(TipoResidencia tipoResidencia);
-
-}
