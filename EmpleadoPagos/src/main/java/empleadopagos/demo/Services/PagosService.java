370500adb8ce8beb9cf3db8bd359ee7fca682307
danrecarte <109390974+danrecarte@users.noreply.github.com>
2024-04-22 01:40:44 -0600

    Delete EmpleadoPagos directory

--- EmpleadoPagos/src/main/java/empleadopagos/demo/Services/PagosService.java
+++
@@ -1,8 +0,0 @@
-package empleadopagos.demo.Services;
-
-import empleadopagos.demo.Entities.HistoricoPago;
-
-public interface PagosService {
-    
-    public HistoricoPago crearPagos(HistoricoPago historicoPago);
-}
