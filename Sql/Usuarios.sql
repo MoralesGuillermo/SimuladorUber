e294bed6e5a3fa4b0218eb5acb8684dcad1e3660
danrecarte <109390974+danrecarte@users.noreply.github.com>
2024-04-22 01:40:23 -0600

    Delete Sql directory

--- Sql/Usuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuarios (
-    codigoUsuario VARCHAR(20) PRIMARY KEY,
-    nombre VARCHAR(50),
-    apellido VARCHAR(50),
-    departamento VARCHAR(100),
-    correo VARCHAR(90),
-    telefono VARCHAR(20)
-);
